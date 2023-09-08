fbc6b047979e656264c5a9a185701bb997f2f9d7
field <jsw_777@naver.com>
2023-09-08 11:47:11 +0900

    ResponseCheck.html - 3

--- 4.반응속도체크/ResponseCheck.vue
+++ 4.반응속도체크/ResponseCheck.vue
@@ -3,7 +3,7 @@
         <!-- class="state" : #screen.state (X) -->
         <!-- v-bind:class="state" : #screen.waiting (O) -->
         <!-- :class로 쓸 수도 있다 -->
-        <div id="screen" v-bind:class="state">{{ message }}</div> 
+        <div id="screen" v-bind:class="state" @click="onClickScreen">{{ message }}</div>
         <div>
             <div>평균 시간 : {{ }}</div>
             <button @click="onReset">리셋</button>
@@ -21,28 +21,38 @@ export default {
         }
     },
     methods: {
-        onReset(){},
+        onReset() { },
+        onClickScreen() {
+            if (this.state === 'waiting') {
+                this.state = 'ready';
+            } else if (this.state === 'ready') {
+                this.state = 'now';
+            } else if (this.state === 'now') {
+                this.state = 'waiting';
+            }
+        },
     }
 };  
 </script>
 
 <!-- !You may need an additional loader to handle the result of these loaders -->
 <!-- scoped : 이 CSS는 이 컴포넌트 안에서만 유효합니다 -->
-<style scoped> 
-    #screen {
-        width: 300px;
-        height: 200px;
-        text-align: center;
-        user-select: none;
-    }
-    #screen.waiting { 
-        background-color: cornflowerblue;
-    }
-    #screen.ready {
-        background-color: crimson;
-        color: white;
-    }
-    #screen.now {
-        background-color: gold;
-    }
-</style>
+<style scoped> #screen {
+     width: 300px;
+     height: 200px;
+     text-align: center;
+     user-select: none;
+ }
+
+ #screen.waiting {
+     background-color: cornflowerblue;
+ }
+
+ #screen.ready {
+     background-color: crimson;
+     color: white;
+ }
+
+ #screen.now {
+     background-color: gold;
+ }</style>
--- 4.반응속도체크/webpack.config.js
+++ 4.반응속도체크/webpack.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
+    devServer: {
+        devMiddleware: { publicPath: "/dist/" },
+        static: { directory: path.resolve(__dirname) },
+    },
     devtool: 'eval',
     resolve: {
         extensions: ['.js', '.vue'],
