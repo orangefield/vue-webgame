e2277139a95b15e658d479f9fef1426c786ecc90
field <jsw_777@naver.com>
2023-09-08 16:36:28 +0900

    RockScissorsPaper.html - 3

--- 5.가위바위보/RockScissorsPaper.vue
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -17,6 +17,9 @@ const rspCoords = {
     가위: '-459px',
     보: '-815px'
 }
+
+let interval = null;
+
 export default {
     data() {
         return {
@@ -35,7 +38,35 @@ export default {
     },
     methods: {
         onClickButton(choice) { },
-    }
+    },
+    /*
+    beforeCreate() { console.log('beforeCreate'); },
+    created() { console.log('created'); },
+    beforeMount() { console.log('beforeMount'); },
+    */
+    mounted() {
+        // console.log('mounted');
+        interval = setInterval(() => {
+            if (this.imgCoord === rspCoords.바위) {
+                this.imgCoord = rspCoords.가위;
+            } else if (this.imgCoord === rspCoords.가위) {
+                this.imgCoord = rspCoords.보;
+            } else if (this.imgCoord === rspCoords.보) {
+                this.imgCoord = rspCoords.바위;
+            }
+        }, 100); // 0.1초
+    },
+    /*
+    beforeUpdate() { console.log('beforeUpdate'); },
+    updated() { console.log('updated'); },
+    */
+    beforeDestroy() {
+        // console.log('beforeDestroy');
+        clearInterval(interval); // 컴포넌트가 사라진 후에도 setInterval()이 계속 실행되는 것을 막기 위해, 메모리 누수를 막기 위해
+    },
+    /*
+    destroyed() { console.log('destroyed'); },
+    */
 };  
 </script>
 
