17a1b532ae667a0ea1b5f4a680b8141c642d1ef2
field <jsw_777@naver.com>
2023-09-08 10:08:11 +0900

    ResponseCheck.html - 2

--- 4.반응속도체크/ResponseCheck.vue
+++ 4.반응속도체크/ResponseCheck.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <div id="screen">{{ message }}</div>
+        <!-- class="state" : #screen.state (X) -->
+        <!-- v-bind:class="state" : #screen.waiting (O) -->
+        <!-- :class로 쓸 수도 있다 -->
+        <div id="screen" v-bind:class="state">{{ message }}</div> 
         <div>
             <div>평균 시간 : {{ }}</div>
             <button @click="onReset">리셋</button>
@@ -9,12 +12,11 @@
 </template>
 
 <script>
-
 export default {
     data() {
         return {
             result: [],
-            state: 'waiting',
+            state: 'waiting', // 클래스랑 연결할 것임
             message: '클릭해서 시작하세요.',
         }
     },
@@ -24,4 +26,23 @@ export default {
 };  
 </script>
 
-<style></style>
+<!-- !You may need an additional loader to handle the result of these loaders -->
+<!-- scoped : 이 CSS는 이 컴포넌트 안에서만 유효합니다 -->
+<style scoped> 
+    #screen {
+        width: 300px;
+        height: 200px;
+        text-align: center;
+        user-select: none;
+    }
+    #screen.waiting { 
+        background-color: cornflowerblue;
+    }
+    #screen.ready {
+        background-color: crimson;
+        color: white;
+    }
+    #screen.now {
+        background-color: gold;
+    }
+</style>
--- 4.반응속도체크/webpack.config.js
+++ 4.반응속도체크/webpack.config.js
@@ -1,30 +1,33 @@
-// webpack.config.js 파일을 직접 생성
-// require : js의 모듈 시스템
-const { VueLoaderPlugin } = require('vue-loader');   // !vue-loader was used without the corresponding plugin.
-const path = require('path');   // !The output directory as **absolute path**
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
 
-// entry, module, plugins, output 중요
-module.exports = {  // node의 모듈 시스템
-    mode: 'development',    // 배포 : 'production'
-    devtool: 'eval',        // 배포시에는 'hidden-source-map'
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
     resolve: {
-        extensions: ['.js', '.vue'],    // 확장자 생략 가능
+        extensions: ['.js', '.vue'],
     },
-    entry: {    // 엄청 많은 스크립트 중에 가장 대표가 될 파일(app.js : 엄청 많은 스크립트들이 하나로 합쳐지는 파일)
+    entry: {
         app: path.join(__dirname, 'main.js'),
     },
-    module: {   // 웹팩의 핵심
-        rules: [{   // 어떻게 합칠건지
-            // !export 'default' (imported as 'Vue' was not found in 'vue')
-            test: /\.vue$/,     // .vue 파일들은 웹팩이 아니라 뷰로더가 맡겠다
-            use: 'vue-loader',   // use 대신 loader 써도 됨
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: 'vue-loader',
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+            ]
         }],
     },
     plugins: [
         new VueLoaderPlugin(),
     ],
     output: {
-        filename: 'app.js',  // entry의 결과물('[name].js'라 써도 된다)
-        path: path.join(__dirname, 'dist'), // __dirname : 현재 폴더
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist'),
     },
 };
