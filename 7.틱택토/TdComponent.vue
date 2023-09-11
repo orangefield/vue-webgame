fe82da46562b5f69dd888c3aa6718fc4d6d3f8ba
field <jsw_777@naver.com>
2023-09-11 13:32:18 +0900

    TicTacToe.html - 1

---
+++ 7.틱택토/TdComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <td>{{ cellData }}</td>
+</template>
+
+<script>
+export default {
+    props: {
+        cellData: String, // 'O', 'X'
+        cellIndex: Number,
+        rowIndex: Number,
+    }
+}
+</script>
---
+++ 7.틱택토/TicTacToe.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       <table-component :table-data="tableData" />
+    </div>
+</template>
+
+<script>
+import TableComponent from './TableComponent';
+
+export default {
+    components: { 
+        TableComponent,
+    },
+    data() {
+        return {
+           tableData: [ // 2차원 배열 : 배열 안에 배열
+            ['', '', ''], 
+            ['', '', ''], 
+            ['', '', ''], 
+           ],
+        };
+    },
+    methods: {
+       
+    },
+}; 
+</script>
+
+<style></style>
