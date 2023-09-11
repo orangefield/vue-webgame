e733b89e6f9052461925bdc0930a15f510050940
field <jsw_777@naver.com>
2023-09-11 16:45:43 +0900

    TicTacToe.html - 5

---
+++ 7.틱택토Vuex/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';  // package.json의 vue를 쓰겠다
+import TicTacToe from './TicTacToe';
+
+createApp(TicTacToe).mount('#root');
---
+++ 7.틱택토Vuex/store.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex';
+
+// state, getters, mutations, actions 중요
+export default createStore({
+    state: { // vue의 data와 비슷
+
+    },
+    getters: { // vue의 computed와 비슷
+
+    },
+    mutations: { // state를 수정할 때 사용(동기적)
+
+    },
+    actions: { // 비동기를 사용할 때 또는 여러 뮤테이션을 연달아 실행할 때
+        
+    },
+});
