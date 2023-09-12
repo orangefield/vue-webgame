d0f98accac44358f8123ac17f92cce7a8aca1e44
field <jsw_777@naver.com>
2023-09-12 17:07:36 +0900

    MineSweeper.html - 4

--- 8.지뢰찾기/MineSweeper.vue
+++ 8.지뢰찾기/MineSweeper.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <mine-form></mine-form> <!-- 지뢰 테이블 설정하는 폼 -->
-        <div>{{ timer }}</div>
+        <br />
+        <div>경과시간 : {{ timer }}</div>
         <table-component></table-component> <!-- 실제 보여질 테이블 -->
         <div>{{ result }}</div>
     </div>
@@ -9,9 +10,11 @@
 
 <script>
 import { mapState } from 'vuex'
-import store from './store';
+import store, { INCREMENT_TIMER } from './store';
 import TableComponent from './TableComponent';
 import MineForm from './MineForm';
+
+let interval;
 
 export default {
     store,
@@ -20,10 +23,21 @@ export default {
         MineForm,
     },
     computed: {
-        ...mapState(['timer', 'result']),
+        ...mapState(['timer', 'result', 'halted']),
     },
     methods: {
 
+    },
+    watch: { // 통제된 환경에서는 써도 된다
+        halted(value, oldValue) {
+            if (value === false) { // false : 게임 시작
+                interval = setInterval(() => {
+                    this.$store.commit(INCREMENT_TIMER)
+                }, 1000);  // js의 타이머는 1초가 진짜 1초가 아닐 수 있다
+            } else { // 게임 중단
+                clearInterval(interval);
+            }
+        },
     },
 }; 
 </script>
